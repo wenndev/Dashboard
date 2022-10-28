2219a8a8195cd81aba059c5716459bf8870128a4
Wendel <delldev7@gmail.com>
2022-10-28 17:56:31 -0300

    add new commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,45 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
-
-
+const themeToggler = document.querySelector(".theme-toggler");
 
 
 // show sidebar
-menuBtn.addEventListener('click',() => {
+menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 });
 
 // close sidebar
-closeBtn.addEventListener('click',() => {
+closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
 });
 
-
 // theme
-const themeToggler = document.querySelector(".theme-toggler");
-themeToggler.addEventListener('click',() => {
-    document.body.classList.toggle('.dark-theme-variables');
+themeToggler.addEventListener('click', () => {
+
+    // error
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').
+    classList.toggle('active');
+
+    themeToggler.querySelector('span:nth-child(2)').
+    classList.toggle('active');
+
+});
+
+// orders in table
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+            <td>${order.productName}</td>
+            <td>${order.productNumber}</td>
+            <td>${order.paymentStatus}</td>
+            <td class=" ${order.shipping === 'Declined' ? 'Danger' : order.shipping === 'pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+            <td class="primary">Details</td>
+        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+
 });
---
+++ assets/js/orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus:'Due',
+        shipping:'Pedding'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName: 'Ruko f11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus:'Due',
+        shipping:'Pedding'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName: 'Gps 4K Drone',
+        productNumber: '22821',
+        paymentStatus:'paid',
+        shipping:'Delivered'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '81475',
+        paymentStatus:'Due',
+        shipping:'Pedding'
+    },
+]
